d7e15b23fb69f1d7ca6bb6e6eb63c32454f9aaa1
thivh <nizamilutvie@gmail.com>
2020-11-08 16:38:00 +0700

    added result page

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ def about():
 @app.route("/motivational_video")
 def motivational_video():
 	return render_template("video.html")
+	
+@app.route("/result")
+def result():
+	return render_template("result.html")
 
 if (__name__ == "__main__"):
 	app.run(debug=True,host="0.0.0.0",port=80)
