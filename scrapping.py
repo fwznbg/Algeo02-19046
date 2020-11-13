dda77d97b2d36b9119d479ddf9aa6bcef343c29b
fwznbg <fnaabigh@gmail.com>
2020-11-13 15:32:42 +0700

    add removePunctuation di scrapping

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+# function for removing punctuation on title used for file's name
+def removePunctuation(sentence):
+    import re
+    # lowercasing
+    sentence = sentence.lower()
+    return re.sub(r'[^\w\s]', '', sentence)
+
 # url to be scrapped
 url = "https://techcrunch.com/2020/11/12/macos-apps-wont-launch/"
 response = requests.get(url)
@@ -14,11 +21,11 @@ title = soup.findAll("h1", class_="article__title")
 paragraphs = soup.findAll('div', class_="article-content")
 
 # make new file and write the title to it
-f = open("./static/simplesearchengine/{}.txt".format(title[0].text.split(":")[0]), "x")
+f = open("./static/simplesearchengine/{}.txt".format(removePunctuation(title[0].text)), "x")
 for x in title:
     f.write(x.get_text())
 f.close()
 
-with open("./static/simplesearchengine/{}.txt".format(title[0].text.split(":")[0]), "a", encoding="utf-8") as f:
+with open("./static/simplesearchengine/{}.txt".format(removePunctuation(title[0].text)), "a", encoding="utf-8") as f:
     for x in paragraphs:
         f.write(x.text)
